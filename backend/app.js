065d2f030b1de1800d108ebc9ca63e401c700500
Paola <20230523@ricaldone.edu.sv>
2025-03-20 09:36:04 -0300

    CRUD suppliers, branches and brand

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 //Importo todo lo de la libreria express
 import express from "express";
 import categoriesRoutes from "./src/routes/categories.js";
+import shoppingCartRoutes from "./src/routes/shoppingCart.js";
+import ordersRoutes from "./src/routes/orders.js";
+import employeesRoutes from "./src/routes/employees.js";
+import brandsRoutes from "./src/routes/brands.js";
+import branchesRoutes from "./src/routes/branches.js";
+import suppliersRoutes from "./src/routes/suppliers.js";
+
+
 
 //Cree una constante que es igual a la libreria
 //que acabo de de importar, y la ejecuto
@@ -10,6 +18,13 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/categories", categoriesRoutes);
+app.use("/api/shoppingCart", shoppingCartRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/employees", employeesRoutes);
+app.use("/api/brands", brandsRoutes);
+app.use("/api/branches", branchesRoutes);
+app.use("/api/suppliers", suppliersRoutes)
+
 
 //exporto esta constante para usar express en todos lados 
 export default app;
